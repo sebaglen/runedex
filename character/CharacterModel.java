ffde21f4c11305b60e3661e10c9eff6c6315e01f
Sebastian Aglen Danielsen <31412302+sebaglen@users.noreply.github.com>
2020-07-06 15:21:42 +0200

    Initial Commit

---
+++ character/CharacterModel.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.runedex.character;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class CharacterModel {
+    private final int testInt;
+}
