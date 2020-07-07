25f8e9e22aa02b5d5ff899b79dd86439818dc41b
Sebastian Aglen Danielsen <sd@udp.no>
2020-07-07 12:09:05 +0200

    removed unused Map import

--- APIManager.java
+++ APIManager.java
@@ -28,7 +28,6 @@ package net.runelite.client.plugins.runedex;
 import javax.inject.Singleton;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import com.google.gson.Gson;
